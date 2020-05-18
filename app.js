a2e0de4210e3b649fbad612e37590f14655a85c4
JoRan29 <jokavana29@gmail.com>
2020-05-18 22:34:09 +0200

    scss grid

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let formKorisnik = document.querySelector("#formKorisnik");
 let formPredlog = document.querySelector("#formPredlog");
 
 console.log(formKorisnik, formPredlog);
-// Klasa 
+// Klasa
 class Geografija {
-    constructor(kor, kat, poj, slo, vreme);
+  constructor(kor, kat, poj, slo, vreme) {}
 }
