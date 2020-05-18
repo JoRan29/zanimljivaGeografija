8b8909e157ef1ca3a14f60bde6ce6afd10df6cb2
JoRan29 <jokavana29@gmail.com>
2020-05-18 22:16:42 +0200

    main html

---
+++ app.js
@@ -0,0 +1,9 @@
+// DOM
+let formKorisnik = document.querySelector("#formKorisnik");
+let formPredlog = document.querySelector("#formPredlog");
+
+console.log(formKorisnik, formPredlog);
+// Klasa 
+class Geografija {
+    constructor(kor, kat, poj, slo, vreme);
+}
