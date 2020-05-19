054c2cc4155c160b1ee749c135fb8d65afa28705
JoRan29 <jokavana29@gmail.com>
2020-05-20 01:47:50 +0200

    regEx + velikoSlovo

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ let formKorisnik = document.querySelector("#formKorisnik");
 let inputKorisnik = document.querySelector("#korisnik");
 let formPredlog = document.querySelector("#formPredlog");
 let inputPredlog = document.querySelector("#predlog");
+let inputSelect = document.querySelector("#select");
+let pozdrav = document.querySelector("#pozdrav");
+// console.log(pozdrav);
+// console.log(formKorisnik, formPredlog);
 
-console.log(formKorisnik, formPredlog);
 // Klasa
 class Geografija {
   constructor(kor, kat, poj, slo) {
@@ -39,17 +42,13 @@ class Geografija {
   set slovo(s) {
     this._pocetnoSlovo = s;
   }
-  // metodi
-  ispis() {
-    console.log(this.zgeografija);
-  }
   // metod za dodavanje novog pojma
-  async dodajPojam(poj) {
+  async dodajPojam(poj, kat) {
     let date = new Date();
 
     let predlog = {
       korisnik: this.korisnik,
-      kategorija: this.kategorija,
+      kategorija: kat,
       pojam: poj,
       pocetnoSlovo: this.prvoSlovo(poj),
       vreme: firebase.firestore.Timestamp.fromDate(date),
@@ -59,17 +58,43 @@ class Geografija {
     return response;
   }
   // metod za proveru pojma
-  async proveriPojam(p) {}
+  async proveriPojam(p) {
+    let count = 0;
+    this.zgeografija
+      .get()
+      .then((snap) => {
+        snap.forEach((doc) => {
+          console.log(doc.data().pojam);
+          let pojam = doc.data().pojam;
+          if (pojam == p) {
+            console.log("Exist");
+            count += 1;
+          } else {
+            console.log("Does not exist");
+          }
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    // return false;
+    console.log(count);
+    return count;
+  }
   // metod za promenu korisnika
   promeniKorisnika(korisnik) {
     this.korisnik = korisnik;
     localStorage.setItem("korisnik", korisnik);
   }
-  // metod za pocetno veliko slovo
+  // metod za pocetno slovo
   prvoSlovo(poj) {
     let pocetnoSlo = poj.slice(0, 1);
     pocetnoSlo.toUpperCase();
     return pocetnoSlo;
+  }
+  // metod za postavljanje velikog prvog slova od predlozene reci
+  veliko(predlog) {
+    return predlog.charAt(0).toUpperCase() + predlog.slice(1);
   }
 }
 
@@ -77,8 +102,10 @@ class Geografija {
 // get username from local memory
 let korisnik = () => {
   if (localStorage.korisnik) {
+    pozdrav.innerHTML = `Hello, ${localStorage.korisnik}!`;
     return localStorage.korisnik;
   } else {
+    pozdrav.innerHTML = `Morate se ulogovati da nastavite igru!`;
     return "anonimus";
   }
 };
@@ -96,6 +123,18 @@ formKorisnik.addEventListener("submit", (e) => {
 // dodaj pojam
 formPredlog.addEventListener("submit", (e) => {
   e.preventDefault();
+  let predlog = inputPredlog.value;
+  let kategorija = inputSelect.value;
+  let patternReg = /^\S*$/;
+  if (patternReg.test(predlog) && predlog.length) {
+    predlogCap = zgeo.veliko(predlog);
+    console.log("Predlog odgovara!" + predlogCap);
+    // dodavanje u bazu
+    // zgeo.dodajPojam(predlog, kategorija);
+    // console.log("Novi pojam dodat u bazu!");
+  } else {
+    console.log("Predlog ne sme sadrzati prazne karaktere!");
+  }
 });
 
 // bez korisnickog imena
@@ -103,4 +142,5 @@ if (!localStorage.korisnik) {
   console.log(`Izaberi korisnicko ime: `);
   // document.body.style.innerHTML = "";
   document.body.style.pointerEvents = "none";
+  alert(`Ne mozete pristupiti stranici bez korisnickog imena!`);
 }
